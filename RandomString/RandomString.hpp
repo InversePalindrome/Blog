67410b70f6778a6ab10ba877e21748fa9ebc3434
Bryan Triana <bryantrianarueda@gmail.com>
2019-06-24 16:51:40 -0400

    Code snippet for Random String blogpost

---
+++ RandomString/RandomString.hpp
@@ -0,0 +1,30 @@
+/*
+Copyright (c) 2019 Inverse Palindrome
+Blog - RandomString.hpp
+https://inversepalindrome.com/
+*/
+
+
+#pragma once
+
+#include <string>
+#include <random>
+
+
+std::string random_string(std::size_t length)
+{
+    const std::string characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    std::random_device random_device;
+    std::mt19937 generator(random_device());
+    std::uniform_int_distribution<> distribution(0, characters.size() - 1);
+
+    std::string random_string;
+
+    for (std::size_t i = 0; i < length; ++i)
+    {
+        random_string += characters[distribution(generator)];
+    }
+
+    return random_string;
+}
